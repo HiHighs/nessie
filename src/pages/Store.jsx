26ae83a50efd76a191bc345968062d9cc6ca60d5
HiHighs <degroote.jonas@hotmail.com>
2024-11-28 17:12:31 +0100

    Temporarily disable store

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,39 +1,39 @@
-import { useCart } from '../Components/CartContext/CartContext'; // Import the useCart hook
+// import { useCart } from '../Components/CartContext/CartContext';
 import Navigation from '../Components/Navigation/Navigation';
 import Footer from '../Components/Footer/Footer';
-import CartIcon from '../Components/CartIcon/CartIcon'; // Import CartIcon
-import styles from './Store.module.css';
-import Product from '../Components/Product/Product';
+// import CartIcon from '../Components/CartIcon/CartIcon'; // Import CartIcon
+// import styles from './Store.module.css';
+// import Product from '../Components/Product/Product';
 
 // Mock Product Data
-const products = [
-  {
-    id: 1,
-    name: 'Flowers White',
-    price: 29.99,
-    image: '/assets/Store/1_flowerswhite.png',
-  },
-  {
-    id: 2,
-    name: 'Flowers Black',
-    price: 49.99,
-    image: '/assets/Store/2_flowersblack.png',
-  },
-  {
-    id: 3,
-    name: 'Nessie',
-    price: 19.99,
-    image: '/assets/Store/3_nessie.png',
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     name: 'Flowers White',
+//     price: 29.99,
+//     image: '/assets/Store/1_flowerswhite.png',
+//   },
+//   {
+//     id: 2,
+//     name: 'Flowers Black',
+//     price: 49.99,
+//     image: '/assets/Store/2_flowersblack.png',
+//   },
+//   {
+//     id: 3,
+//     name: 'Nessie',
+//     price: 19.99,
+//     image: '/assets/Store/3_nessie.png',
+//   },
+// ];
 
 function Store() {
-  const { addToCart, removeFromCart } = useCart();
+  // const { addToCart, removeFromCart } = useCart();
 
   return (
     <div>
       <Navigation />
-      <CartIcon />
+      {/* <CartIcon />
       <h1 className={styles.fadeIn}>Store</h1>
       <div className={styles.productList}>
         {products.map((product) => (
@@ -44,7 +44,8 @@ function Store() {
             onRemoveFromCart={() => removeFromCart(product.id)}
           />
         ))}
-      </div>
+      </div> */}
+      <h1>COMING SOON</h1>
       <Footer />
     </div>
   );
